ef912c7c22b7779abf0d8092809804a4fdd700a1
Alex Hing Liu <hingliu6@gmail.com>
2019-05-08 19:51:21 -0700

    paddles and board line updated

--- src/settings.js
+++ src/settings.js
@@ -1,1 +1,10 @@
 export const SVG_NS = "http://www.w3.org/2000/svg";
+
+export const PADDLE_WIDTH = 8, PADDLE_HEIGHT = 56, BOARD_GAP = 10;
+export const KEYS = {
+    p1up: 'w',
+    p1down: 's',
+    p2down: 'ArrowUp',
+    p2down: 'ArrowDown',
+    pause: ' '
+};
