d8c01aebf537dc0bb5c2698bcd90d4141e619e5f
Alex Hing Liu <hingliu6@gmail.com>
2019-05-14 18:04:21 -0700

    read me file updated

--- README.md
+++ README.md
@@ -1,41 +1,16 @@
 # Pong Game Starter
 
-A starter project for a basic pong game using SVGs.
+A starter project for a basic pong game using SVGs and JavaScript. 
 
-## Setup
+# Tools applied
 
-Ensure you have [Node.js](https://nodejs.org/en/) installed first.
+- Github
+- Javascript
+- Objects and arrays
+- Operators
+- Variables
+- Beautify
 
-**Install dependencies:**
+# Personal learnings
 
-`$ npm install`
-
-**Run locally with the Parcel dev server:**
-
-`$ npm start`
-
-Once you run the start command you can access your project at http://localhost:3000.
-
-Read more about the [Parcel web application bundler here](https://parceljs.org/).
-
-## Deploy
-
-The deployment workflow for this project will be a bit different from what you've used when deploying simple static websites.
-
-To deploy your finished Pong project as a GitHub page, you must first **update the `predeploy` script in the `package.json` file with the name of your repo.**
-
-For example, if your repo URL is:
-
-https://github.com/bob/pong-project
-
-Update the `predeploy` script as follows:
-
-```json
-"predeploy": "rm -rf dist && parcel build index.html --public-url /pong-project",
-```
-
-Once you have done this, you can run:
-
-`$ npm run deploy`
-
-Now check out your deployed site 🙂
+The project was crucial to understand how classes and objects behave and allow you to implement complex solutions to your ideas.
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,5 +1,12 @@
-
-import { SVG_NS, PADDLE_WIDTH, PADDLE_HEIGHT, BOARD_GAP, KEYS, RADIUS, SPEED } from '../settings';
+import {
+  SVG_NS,
+  PADDLE_WIDTH,
+  PADDLE_HEIGHT,
+  BOARD_GAP,
+  KEYS,
+  RADIUS,
+  SPEED
+} from '../settings';
 import Board from './Board';
 import Paddle from './Paddle';
 import Ball from './Ball';
@@ -29,18 +36,18 @@ export default class Game {
     this.ball2 = new Ball2(this.width, this.height, RADIUS);
     this.ball3 = new Ball3(this.width, this.height, RADIUS);
     this.ball4 = new Ball4(this.width, this.height, RADIUS);
-    this.score1 = new Score(this.width/2 - 50, 30);
-    this.score2 = new Score(this.width/2 + 25, 30);  
-    
+    this.score1 = new Score(this.width / 2 - 50, 30);
+    this.score2 = new Score(this.width / 2 + 25, 30);
+
     document.addEventListener("keydown", (event) => {
-      if(event.key === KEYS.pause) {
+      if (event.key === KEYS.pause) {
         this.paused = !this.paused;
         this.paddle1.setSpeed(SPEED);
         this.paddle2.setSpeed(SPEED);
       }
     });
-  
-		// Other code goes here...
+
+    // Other code goes here...
   }
 
   render() {
@@ -58,7 +65,7 @@ export default class Game {
     this.board.render(svg);
     this.paddle1.render(svg);
     this.paddle2.render(svg);
-    this.ball.render(svg, this.paddle1, this.paddle2);  
+    this.ball.render(svg, this.paddle1, this.paddle2);
     this.ball1.render(svg, this.paddle1, this.paddle2);
     this.ball2.render(svg, this.paddle1, this.paddle2);
     this.ball3.render(svg, this.paddle1, this.paddle2);
@@ -73,7 +80,7 @@ export default class Game {
       const p = document.querySelectorAll('p');
       p[0].setAttribute('style', 'display: none');
       p[1].setAttribute('style', 'display: none');
-  
+
     } else if (this.paddle2.getScore() === 10) {
       this.paused = !this.paused;
       this.gameElement.innerHTML = 'Player 2 wins';
@@ -81,11 +88,5 @@ export default class Game {
       p[0].setAttribute('style', 'display: none');
       p[1].setAttribute('style', 'display: none');
     }
-
   }
-
-
-   
-		// More code goes here....
-  
 }
